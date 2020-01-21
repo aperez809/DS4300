c3fb4b4960b84618eb48572fd2785e569203122d
Aasish12 <aasish6648@gmail.com>
2020-01-21 14:55:31 -0500

    pushing load script

--- HW1/Twitter/src/API/Twitter.java
+++ HW1/Twitter/src/API/Twitter.java
@@ -3,6 +3,13 @@ package API;
 public class Twitter {
     private static TFDatabaseAPI api = new TFDatabaseMysql();
 
-    public static void main(String[] args) throw Exception{
+    public static void main(String[] args){
+        BufferedReader csvReader = new BufferedReader(new FileReader(tweets.csv))
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            Tweet recent = new Tweet(data[0], data[1], data[2], data[3]);
+            insertTweet(recent);
+        }
+        csvReader.close();
     }
 }
